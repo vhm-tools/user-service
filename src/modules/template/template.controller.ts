f704dc935b4fd909e4a445ba6db18c0f3b9498dc
Vũ Huỳnh Minh <36500047+vhm205@users.noreply.github.com>
2023-10-21 17:05:48 +0700

    chore: update template module

--- src/modules/template/template.controller.ts
+++ src/modules/template/template.controller.ts
@@ -1,13 +1,22 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { IResponseType } from '@share-libs';
+import { CreateTemplateDto } from './dtos';
 import env from '@environments';
 
-@Controller(`v${env.API_VERSION}/template`)
+@Controller({
+  path: 'template',
+  version: env.API_VERSION,
+})
 @ApiTags('Template')
 export class TemplateController {
   @Post('create')
-  @HttpCode(HttpStatus.ACCEPTED)
-  sendMail(@Body() payload: any): Promise<any> {
-    return Promise.resolve(payload);
+  @HttpCode(HttpStatus.OK)
+  create(
+    @Body() payload: CreateTemplateDto,
+  ): Promise<IResponseType<CreateTemplateDto>> {
+    return Promise.resolve({
+      data: payload,
+    });
   }
 }
